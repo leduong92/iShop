5dd740640ab45cc4c00aefe0d58d83ed415ae896
Mai Le Duong <maileduong92@gmail.com>
2020-11-07 16:13:25 +0700

    Add project files.

---
+++ iShop.BackendApi/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iShop.BackendApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "This will be a list of products";
+        }
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "Sigle product";
+        }
+    }
+}
