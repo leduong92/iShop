3cfbaf1b473db0dac88f60d772c6169a2025108f
Mai Le Duong <maileduong92@gmail.com>
2020-11-16 13:50:46 +0700

    sorting

--- iShop.BackendApi/Controllers/ProductsController.cs
+++ iShop.BackendApi/Controllers/ProductsController.cs
@@ -29,9 +29,9 @@ namespace iShop.BackendApi.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
+        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(string sort)
         {
-            var spec = new ProductWithTypesAndBrandsSpecification();
+            var spec = new ProductWithTypesAndBrandsSpecification(sort);
             var products = await _productsRepo.ListAsync(spec);
 
             return Ok(
--- iShop.Core/Specification/ProductWithTypesAndBrandsSpecification.cs
+++ iShop.Core/Specification/ProductWithTypesAndBrandsSpecification.cs
@@ -8,11 +8,27 @@ namespace iShop.Core.Specification
 {
     public class ProductWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
-        public ProductWithTypesAndBrandsSpecification()
+        public ProductWithTypesAndBrandsSpecification(string sort)
         {
             AddInclude(x => x.ProductBrand);
             AddInclude(x => x.ProductType);
             AddOrderBy(x => x.Name);
+
+            if (!string.IsNullOrEmpty(sort))
+            {
+                switch (sort)
+                {
+                    case "priceAsc":
+                        AddOrderBy(p => p.Price);
+                        break;
+                    case "priceDesc":
+                        AddOrderByDescending(p => p.Price);
+                        break;
+                    default:
+                        AddOrderBy(n => n.Name);
+                        break;
+                }
+            }
         }
 
         public ProductWithTypesAndBrandsSpecification(int id) : base(x => x.Id == id)
