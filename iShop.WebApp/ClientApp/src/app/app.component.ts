e54043dea8c1f64484c1f0918cbd26024f0bebdd
Mai Le Duong <maileduong92@gmail.com>
2020-11-17 14:49:01 +0700

    using scss implement nav-bar html

--- iShop.WebApp/ClientApp/src/app/app.component.ts
+++ iShop.WebApp/ClientApp/src/app/app.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { error } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+  title = 'iShop App';
+  products: any[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    
+    this.http.get("https://localhost:5001/api/products?pageSize=50").subscribe((Response: any) => {
+      this.products = Response.data;
+      console.log(this.products);
+    }, error => {
+        console.log(error);
+    });
+  }
 }
