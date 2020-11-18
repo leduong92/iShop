6d43199afc81f4a2730cbc4df2e16316d9a9fa07
Mai Le Duong <maileduong92@gmail.com>
2020-11-18 15:48:24 +0700

    complete fix bug

---
+++ iShop.WebApp/ClientApp/src/app/shop/shop.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { ShopService } from './shop.service';
+import { IProduct } from '../shared/modules/product';
+import { IBrand } from '../shared/modules/brands';
+import { IType } from '../shared/modules/productTypes';
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.scss']
+})
+export class ShopComponent implements OnInit {
+
+  products: IProduct[];
+  brands: IBrand[];
+  types: IType[];
+  
+  brandIdSelected = 0;
+  typeIdSelected = 0;
+  sortSelected = 'name';
+  sortOptions = [
+    { name: 'Alphabetical', value: 'name' },
+    { name: 'Price: Low to High', value: 'priceAsc' },
+    { name: 'Price: High to Low', value: 'PriceDesc' },
+  ]
+
+  constructor(private shopService: ShopService) { }
+
+  ngOnInit() {
+    this.getProduct();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProduct() {
+    this.shopService.getProducts(this.brandIdSelected, this.typeIdSelected, this.sortSelected).subscribe(response => {
+      this.products = response.data;
+    }, error => {
+      console.log(error);
+    })
+  }
+  getBrands() {
+    this.shopService.getBrands().subscribe(response => {
+      this.brands = [{ id: 0, name: 'All' }, ...response];
+    }, error => {
+      console.log(error);
+    })
+  }
+  getTypes() {
+    this.shopService.getTypes().subscribe(response => {
+      this.types = [{ id: 0, name: 'All' }, ...response];
+    }, error => {
+      console.log(error);
+    })
+  }
+
+
+  onBrandSelected(brandId: number) {
+    this.brandIdSelected = brandId;
+    this.getProduct();
+  }
+  onTypeSelected(typeId: number) {
+    this.typeIdSelected = typeId;
+    this.getProduct()
+  }
+  onSortSelected(sort: string) {
+    this.sortSelected = sort;
+    this.getProduct();
+  }
+
+}
