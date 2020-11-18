5ea3a33fbefd57b297b7624337321ab2303a4790
Mai Le Duong <maileduong92@gmail.com>
2020-11-18 15:59:03 +0700

    add pagination

--- iShop.WebApp/ClientApp/src/app/shop/shop.module.ts
+++ iShop.WebApp/ClientApp/src/app/shop/shop.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductItemsComponent } from './product-items/product-items.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ShopComponent, ProductItemsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [ShopComponent]
 })
